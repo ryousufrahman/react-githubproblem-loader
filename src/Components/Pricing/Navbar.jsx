d8c5bd5bb6b0684aa968b5bd66e7e5492c4cbc52
yousufrahman <ryousuf766@gmail.com>
2026-03-19 13:09:22 +0600

    added the toggle menu

--- src/Components/Pricing/Navbar.jsx
+++ src/Components/Pricing/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { CircleX, Menu } from "lucide-react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+    const [open, setOpen]= useState(false)
   return (
     <div className="max-w-300 mx-auto mt-5">
       <nav className="flex justify-between items-center">
         {/* navleft */}
-        <div className="">
+        <div className="flex gap-4 items-center  ">
+           <div  onClick={()=>setOpen(!open)}>
+               {
+                open?<CircleX className="md:hidden"></CircleX>  : <Menu className="md:hidden " ></Menu>
+             }
+           
+           </div>
+            
           <h1 className="text-5xl text-primary">NAVBAR</h1>
           <div>
             
