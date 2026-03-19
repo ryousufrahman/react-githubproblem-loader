c18d1145dab8bef21f8e1c5f8176575b0a0c5e08
yousufrahman <ryousuf766@gmail.com>
2026-03-19 16:30:28 +0600

    added the final card

--- src/Components/Pricing/PricingOption/PriceCart.jsx
+++ src/Components/Pricing/PricingOption/PriceCart.jsx
@@ -1,4 +1,4 @@
-import { TicketCheck } from "lucide-react";
+import { CircleCheckBig } from "lucide-react";
 import React from "react";
 
 const Price = ({ data }) => {
@@ -7,23 +7,24 @@ const Price = ({ data }) => {
 
   return (
     <>
-      <div className="bg-amber-200 rounded-2xl p-5 ">
+      <div className="bg-amber-500 rounded-2xl p-5 flex flex-col gap-4 ">
         <div>
-          <h2 className="text-4xl">{data.name}</h2>
+          <h2 className="text-4xl">{data.name} <span className="text-2xl text-gray-500 font-bold">{data.isPopular? 'Recomended' : ''}</span></h2>
           <h4 className="text-2xl">{data.price}</h4>
-          <p>{data.description}</p>
+          
         </div>
 
-        <div className="mt-3 bg-amber-400 p-4 rounded-2xl">
+        <div className="mt-3 bg-amber-200 p-4 rounded-2xl flex-1">
+            <p>{data.description}</p>
           {features.map((feature) => (
             <p className="flex gap-2">
               {" "}
-              <TicketCheck className="text-green-500"></TicketCheck> {feature}
+              <CircleCheckBig className="text-green-500"></CircleCheckBig> {feature}
             </p>
           ))}
         </div>
         <div>
-          <button className="btn w-full btn-primary ">Wide</button>
+          <button className="btn w-full btn-primary ">{data.buttonText}</button>
         </div>
       </div>
     </>
